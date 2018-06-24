a7b56f309b9ec880c19a01b5561a46a0f0b39dac
C17P9ANUFVH3 <computer@C17P9ANUFVH3s-MacBook-Pro.local>
2018-06-24 13:20:24 -0500

    Added tooltips to dashboard headers

--- src/components/dashboard/CategoryChartGroup/CategoryChartGroup.jsx
+++ src/components/dashboard/CategoryChartGroup/CategoryChartGroup.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-import { Col } from 'react-bootstrap'
+import { Col, Tooltip, OverlayTrigger } from 'react-bootstrap'
 import './CategoryChartGroup.css'
 
 import { PercentCircle } from '../../graph/PercentCircle'
 
 export const CategoryChartGroup = ({chartData}) => (
+  
   <Col xs={ 12 } sm={ 4 } className="CategoryChartGroup">
-    <h3 className="CategoryChartGroup-title">{ chartData.name }</h3>
+    <OverlayTrigger
+      placement="bottom"
+      overlay={tooltip(chartData)}
+      delayHide={ 150 }
+    >
+      <h3 className="CategoryChartGroup-title">{ chartData.name }</h3>
+    </OverlayTrigger>
     <PercentCircle percent={chartData.percent} />
     <h5 className="CategoryChartGroup-percent">{ chartData.percent || 0 }%</h5>
-  </Col> 
+  </Col>
 )
 
 CategoryChartGroup.propTypes = {
   chartData: PropTypes.object.isRequired
+}
+
+function tooltip({name, total, budget}) {
+  return (
+    <Tooltip id={ `tooltip-${name}`}>
+      {`$${total} / $${budget}`}
+    </Tooltip>
+  )
 }
